f9e350a6020d34c8e4911dc4c7f25627822dc932
a-type <gforrest@bandwidth.com>
2017-10-27 16:10:35 -0400

    infer default state by calling the reducer

--- src/facetReducer.js
+++ src/facetReducer.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import hasFacet from '../helpers/hasFacet';
 
-export default (baseDefaultState) => (facetName, reducer, defaultState) => {
-  const filteredReducer = (state = (defaultState || baseDefaultState), action) => {
+export default (facetName, reducer, defaultState) => {
+  const inferredDefaultState =
+    defaultState || reducer(undefined, { type: '@@init' });
+  const filteredReducer = (state = inferredDefaultState, action) => {
     if (hasFacet(facetName)(action)) {
       return reducer(state, action);
     }
--- src/mutable/facetReducer.js
+++
@@ -1,3 +0,0 @@
-import createFacetReducer from '../factories/createFacetReducer';
-
-export default createFacetReducer({});
